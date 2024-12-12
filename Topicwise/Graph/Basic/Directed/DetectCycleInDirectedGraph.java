903533d36d74eb62fd716224a36042ff52529dfd
namannc13 <chawhannaman5@gmail.com>
2024-12-12 11:30:16 +0530

    DetectCycleInDirectedGraphBFSUsingKahn, CourseScheduleII, FindEventualSafeStatesBFSUsingKahn

--- Topicwise/Graph/Basic/Directed/DetectCycleInDirectedGraph.java
+++ Topicwise/Graph/Basic/Directed/DetectCycleInDirectedGraph.java
@@ -1,4 +1,4 @@
-package Topicwise.Graph.Basic.Directed;
+
 
 import java.util.ArrayList;
 
--- Topicwise/Graph/ques/BFS_or_DFS/CourseScheduleII.java
+++ Topicwise/Graph/ques/BFS_or_DFS/CourseScheduleII.java
@@ -1,28 +1,31 @@
+package BFS_or_DFS;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Queue;
 
+// Same as Kahns Algo / Using Kahns Algo
 // TC: O( V + E ) ( directed graph bfs ) + O ( V + E ) ( filling the indegree array )
-// SC: O(V) (indegree[] array) + O(V) (queue) + O(V) (list) => O(V)
+// SC: O(V) (indegree[] array) + O(V) (queue) + O(V) (answer[] array) => O(V)
 
-public class KahnsAlgorithm {
+public class CourseScheduleII {
     public static void main(String[] args) {
-        int V = 6;
+        int numCourses = 4;
+        int[][] prerequisites = {{1,0},{2,0},{3,1},{3,2}};
+        System.out.println();
+        System.out.println(Arrays.toString(findOrder(numCourses, prerequisites)));
+        System.out.println();
+    }
+    public static int[] findOrder(int numCourses, int[][] prerequisites) {
         ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
-
-        for (int i = 0; i < V; i++) {
+        for (int i = 0; i < numCourses; i++) {
             adj.add(new ArrayList<>());
         }
-
-        adj.get(2).add(3);
-        adj.get(3).add(1);
-        adj.get(4).add(0);
-        adj.get(4).add(1);
-        adj.get(5).add(0);
-        adj.get(5).add(2);
-
-        int[] indegree = new int[V];
+        for (int[] arr : prerequisites) {
+            adj.get(arr[0]).add(arr[1]);
+        }
+        int[] indegree = new int[numCourses];
         for(ArrayList<Integer> list: adj){
             for(Integer i: list){
                 indegree[i] += 1;
@@ -34,15 +37,15 @@ public class KahnsAlgorithm {
             if(indegree[i] == 0) q.add(i);
         }
 
-        System.out.println(Arrays.toString(indegree));
         ArrayList<Integer> list = new ArrayList<>();
         bfs(adj, indegree, list, q);
-
-        System.out.println();
-        System.out.println(list);
-        System.out.println();
+        if(list.size() != numCourses) return new int[0];
+        int[] answer = new int[list.size()];
+        for(int i = 0; i < list.size(); i++){
+            answer[i] = list.get(list.size()-1-i);
+        }
+        return answer;
     }
-
     public static void bfs(ArrayList<ArrayList<Integer>> adj, int[] indegree, ArrayList<Integer> list, Queue<Integer> q){
         while(!q.isEmpty()){
             int node = q.poll();
